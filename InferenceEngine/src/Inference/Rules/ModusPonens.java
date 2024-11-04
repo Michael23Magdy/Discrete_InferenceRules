26a78b417666098af51a369e2bd0de9036d7fb44
Michael Magdy <michaelmagdymagdy@gmail.com>
2024-11-04 02:50:25 +0200

    added modus ponens rule

---
+++ InferenceEngine/src/Inference/Rules/ModusPonens.java
@@ -0,0 +1,23 @@
+package Inference.Rules;
+
+import Expression.ExpressionTree;
+
+public class ModusPonens implements InferenceRule{
+
+    @Override
+    public boolean matches(ExpressionTree exp1, ExpressionTree exp2) {
+        return  (exp1.getRoot().getOperator() == '>' &&
+                        exp1.getRoot().getLeftExpression().isEqual(exp2.getRoot())) ||
+                (exp2.getRoot().getOperator() == '>' &&
+                        exp2.getRoot().getLeftExpression().isEqual(exp1.getRoot()));
+    }
+
+    @Override
+    public ExpressionTree apply(ExpressionTree exp1, ExpressionTree exp2) throws Exception {
+        if (exp1.getRoot().getOperator() == '>' && exp1.getRoot().getLeftExpression().isEqual(exp2.getRoot()))
+            return new ExpressionTree(exp1.getRoot().getRightExpression().getExpressionRepresentation());
+
+        return new ExpressionTree(exp2.getRoot().getRightExpression().getExpressionRepresentation());
+
+    }
+}
